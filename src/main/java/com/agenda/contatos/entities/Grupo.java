abea8e264be34fe3083e81327463ac1e78eb9fad
Diego Leão <diegoleaovelame@hotmail.comgit commit -m closes>
2025-12-06 17:41:00 -0300

    closes #2 - Concluído: criação das entities.

---
+++ src/main/java/com/agenda/contatos/entities/Grupo.java
@@ -0,0 +1,73 @@
+package com.agenda.contatos.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "grupos")
+public class Grupo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long Id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    //Constructor padrão sem argumentos, necessário para o JPA.
+    public Grupo() {
+    }
+
+    //Constructor com argumentos, útil para criar novos objetos.
+    public Grupo(String name) {
+        this.name = name;
+    }
+
+    // ---Getters and Setters---
+
+    public Long getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // --- Métodos de Comparação (hashCode e equals) ---
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Grupo other = (Grupo) obj;
+        if (Id == null) {
+            if (other.Id != null)
+                return false;
+        } else if (!Id.equals(other.Id))
+            return false;
+        return true;
+    }   
+}
